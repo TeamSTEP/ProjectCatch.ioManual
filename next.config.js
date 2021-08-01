7fccb8a12550821f0cbeb0b075d81d139fa80674
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-08-01 17:22:21 +0900

    add webpack svg loader (wip)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
+// only if the content is served through a subdirectory (ex: https://www.page.com/content)
 const baseUrl = '';
 
 module.exports = withBundleAnalyzer({
@@ -10,6 +11,21 @@ module.exports = withBundleAnalyzer({
     basePath: baseUrl,
     env: {
         baseUrl: baseUrl,
+    },
+    webpack5: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['preact', 'env'],
+                    },
+                },
+                { loader: '@svgr/webpack' },
+            ],
+        });
+        return config;
     },
     future: {
         webpack5: true,
---
+++ src/hero/keyart/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import BannerLayer1 from './keyart-layer-1.svg';
+import BannerLayer10 from './keyart-layer-10.svg';
+import BannerLayer11 from './keyart-layer-11.svg';
+import BannerLayer12 from './keyart-layer-12.svg';
+import BannerLayer13 from './keyart-layer-13.svg';
+import BannerLayer14 from './keyart-layer-14.svg';
+import BannerLayer2 from './keyart-layer-2.svg';
+import BannerLayer3 from './keyart-layer-3.svg';
+import BannerLayer4 from './keyart-layer-4.svg';
+import BannerLayer5 from './keyart-layer-5.svg';
+import BannerLayer6 from './keyart-layer-6.svg';
+import BannerLayer7 from './keyart-layer-7.svg';
+import BannerLayer8 from './keyart-layer-8.svg';
+import BannerLayer9 from './keyart-layer-9.svg';
+
+// note: not the best way to organize it, but I'm out of ideas ¯\_(ツ)_/¯
+
+export const BannerArtLayers = [
+  <BannerLayer1 />,
+  <BannerLayer2 />,
+  <BannerLayer3 />,
+  <BannerLayer4 />,
+  <BannerLayer5 />,
+  <BannerLayer6 />,
+  <BannerLayer7 />,
+  <BannerLayer8 />,
+  <BannerLayer9 />,
+  <BannerLayer10 />,
+  <BannerLayer11 />,
+  <BannerLayer12 />,
+  <BannerLayer13 />,
+  <BannerLayer14 />,
+];
